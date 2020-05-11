adf819ddf19ffc1053dd95f4672857eae4be8c2e
Alex Ovchinnikov <SanSeyp6@gmail.com>
2020-05-11 04:56:15 +0300

    Moved code for positioning of main window into its constructor

--- src/main/java/ru/hse/todo/swt/controls/MainWindow.java
+++ src/main/java/ru/hse/todo/swt/controls/MainWindow.java
@@ -25,6 +25,12 @@ public final class MainWindow {
 		this.shell.setMinimumSize(640, 390);
 		this.shell.setText("TODO list");
 		this.shell.setLayout(new GridLayout(2, false));
+		Monitor primary = this.display.getPrimaryMonitor();
+		Rectangle bounds = primary.getBounds();
+		Rectangle rect = shell.getBounds();
+		final int x = bounds.x + (bounds.width - rect.width) / 2;
+		final int y = bounds.y + (bounds.height - rect.height) / 2;
+		this.shell.setLocation(x, y);
 		this.listPane = listPane;
 		this.listPane.setParent(this.shell);
 		this.listPane.setLayoutData(new GridData(SWT.FILL, SWT.FILL, false, true, 1, 1));
@@ -35,16 +41,9 @@ public final class MainWindow {
 	}
 
 	/**
-	 * Sets window position to center of encapsulated at constructor {@link Display}
-	 * and shows it.
+	 * Shows encapsulated Shell.
 	 */
 	public void open() {
-		Monitor primary = this.display.getPrimaryMonitor();
-		Rectangle bounds = primary.getBounds();
-		Rectangle rect = shell.getBounds();
-		final int x = bounds.x + (bounds.width - rect.width) / 2;
-		final int y = bounds.y + (bounds.height - rect.height) / 2;
-		this.shell.setLocation(x, y);
 		this.shell.open();
 	}
 
