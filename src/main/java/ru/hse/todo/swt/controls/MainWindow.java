87708540f6399ce385e69d12934274a0e64c2aa1
Alex Ovchinnikov <SanSeyp6@gmail.com>
2020-05-11 04:52:15 +0300

    Removed unused imports

--- src/main/java/ru/hse/todo/swt/controls/MainWindow.java
+++ src/main/java/ru/hse/todo/swt/controls/MainWindow.java
@@ -4,7 +4,6 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Monitor;
 import org.eclipse.swt.widgets.Shell;
